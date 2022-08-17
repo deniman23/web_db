d10f7fa1a4621b8c1ff5a5a31fe613570055a968
Daniil Tolkachev <itdanetollk@gmail.com>
2022-08-17 16:30:44 +0300

    Add

--- web_db/Program.cs
+++ web_db/Program.cs
@@ -1,26 +1,53 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+var builder = WebApplication.CreateBuilder();
+string connection = "Server=(localdb)\\mssqllocaldb;Database=applicationdb;Trusted_Connection=True;";
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
+app.UseDefaultFiles();
 app.UseStaticFiles();
 
-app.UseRouting();
+app.MapGet("/api/users", async (ApplicationContext db) => await db.Users.ToListAsync());
 
-app.UseAuthorization();
+app.MapGet("/api/users/{id:int}", async (int id, ApplicationContext db) =>
+{
+    User? user = await db.Users.FirstOrDefaultAsync(u => u.Id == id);
 
-app.MapRazorPages();
+    if (user == null) return Results.NotFound(new { message = "Пользователь не найден" });
+
+    return Results.Json(user);
+});
+
+app.MapDelete("/api/users/{id:int}", async (int id, ApplicationContext db) =>
+{
+    User? user = await db.Users.FirstOrDefaultAsync(u => u.Id == id);
+
+    if (user == null) return Results.NotFound(new { message = "Пользователь не найден" });
+
+    db.Users.Remove(user);
+    await db.SaveChangesAsync();
+    return Results.Json(user);
+});
+
+app.MapPost("/api/users", async (User user, ApplicationContext db) =>
+{
+    await db.Users.AddAsync(user);
+    await db.SaveChangesAsync();
+    return user;
+});
+
+app.MapPut("/api/users", async (User userData, ApplicationContext db) =>
+{
+    var user = await db.Users.FirstOrDefaultAsync(u => u.Id == userData.Id);
+
+    if (user == null) return Results.NotFound(new { message = "Пользователь не найден" });
+
+    user.Age = userData.Age;
+    user.Name = userData.Name;
+    await db.SaveChangesAsync();
+    return Results.Json(user);
+});
 
 app.Run();
-
